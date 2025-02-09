13cacb4c7ce59c683eec8d9936212ca5627ffd6b
24paketik <87018761+24paketik@users.noreply.github.com>
2025-02-09 11:10:48 +0300

    Сделал обработку разных методов запросов

--- main.go
+++ main.go
@@ -10,8 +10,24 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func methodHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		fmt.Fprintln(w, "Метод GET")
+	case "POST":
+		fmt.Fprintln(w, "Метод POST")
+	case "PUT":
+		fmt.Fprintln(w, "Метод PUT")
+	case "DELETE":
+		fmt.Fprintln(w, "Метод DELETE")
+	default:
+		fmt.Fprintln(w, "Метод не поддерживается")
+	}
+}
+
 func main() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/method", methodHandler)
 	fmt.Println("Сервер запущен на порту 8080")
 	http.ListenAndServe(":8080", nil)
 }
